129ce361b972441bb2b9c10c41373d6ea1787df0
Kizlo Taras <orleon.nilkford@gmail.com>
2018-10-18 23:31:53 +0300

    Add files via upload

---
+++ DataBases/SQL/AuthorSQL.sql
@@ -0,0 +1,17 @@
+﻿-- DROP TABLE "Authors";
+
+CREATE TABLE "Authors"
+(
+	id SERIAL PRIMARY KEY,
+	"name" VARCHAR(30) NOT NULL,
+	surname VARCHAR(30) NOT NULL
+);
+
+INSERT INTO "Authors" 
+	("name", surname)
+VALUES  ('Howard', 'Lovecraft'),
+	('Edgar', 'Poe'),
+	('Abraham', 'Stoker'),
+	('Oscar', 'Wilde');
+
+SELECT * FROM "Authors";
---
+++ DataBases/SQL/PublishingHouseSQL.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE "Publishing house"
+(
+	id SERIAL PRIMARY KEY,
+	"name" VARCHAR(30) NOT NULL UNIQUE,
+	country VARCHAR(30)
+);
+
+INSERT INTO "Publishing house"
+	("name", country)
+VALUES  ('One Day Night','UK'),
+	('4Square','USA'),
+	('Morning','Ukraine'),
+	('Basis','Ukraine');
+
+SELECT * FROM "Publishing house";
+
+SELECT "name" FROM "Publishing house"
+WHERE country = 'Ukraine';
